e5ec58353e67c7bb8c8bffd1e5ce2f048080192a
Rachel Kawula <rkawula@gmail.com>
2015-03-16 13:09:49 -0700

    Generated welcome controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MillsComputerClub::Application.routes.draw do
 
-	get '/', to 'application#index'
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
