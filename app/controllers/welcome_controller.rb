edf08c14a0bf89c99ba51ca74f090db41e29b610
wcheung4710 <wcheung@mills.edu>
2015-04-22 13:25:56 -0700

    singnin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,15 @@
 class WelcomeController < ApplicationController
-  def index
-  	# default to just show the page
-  end
-  def blog
-  end
-  def events
-  end
-  def projects
-  end
-  def achievements
-  end
-  def authors
-  end
+    def index
+        # default to just show the page
+    end
+    def blog
+    end
+    def events
+    end
+    def projects
+    end
+    def achievements
+    end
+    def authors
+    end
 end
