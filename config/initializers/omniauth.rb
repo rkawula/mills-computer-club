73bf401ea68b730f3b98cae2653563fb842c41c0
Rachel Kawula <rkawula@gmail.com>
2015-05-10 18:30:04 -0700

    Attempting to make heroku environment variables work on deployment.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :google_oauth2, ENV['GOOGLE_ID'],
+	ENV['GOOGLE_SECRET'],
+	{
+		hd: 'mills.edu',
+		client_options:
+		{ssl:
+			{ca_file: Rails.root.join("cacert.pem").to_s}}}
+end
