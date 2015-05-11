70fc7651c1e90e31394d7a938e75be2a6c0f1bfe
Rachel Kawula <rkawula@gmail.com>
2015-05-10 18:42:09 -0700

    Removed files causing errors with Heroku deployment.

--- db/migrate/20150430175148_remove_tag_from_resource.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTagFromResource < ActiveRecord::Migration
-  def up
-    remove_column :resources, :tag
-  end
-
-  def down
-    add_column :resources, :tag, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150430175148) do
-
-  create_table "blog_posts", :force => true do |t|
-    t.string   "pid"
-    t.string   "title"
-    t.text     "contents"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20150430172911) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -37,12 +28,14 @@ ActiveRecord::Schema.define(:version => 20150430175148) do
     t.string   "question"
     t.text     "answer"
     t.string   "url"
+    t.string   "tag"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
     t.string   "provider"
+    t.string   "uid"
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
