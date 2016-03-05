1ed5b84c818d0a07b3815cc23e77c275869c9bee
Rachel Kawula <rkawula@gmail.com>
2016-03-04 16:09:01 -0800

    PG and older version of rails: probably need literal strings.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
     
     def index
-        @mcc_officers = Officer.order(created_at: :asc).where(undergrad: true)
-        @mice_officers = Officer.order(created_at: :asc).where(undergrad: false)
+        @mcc_officers = Officer.order("created_at asc").where(undergrad: true)
+        @mice_officers = Officer.order("created_at asc").where(undergrad: false)
     end
 
     def events
