d1c9d5fc8d79a4c491a4bf896c486e9fbcc4589b
Rachel Kawula <rkawula@gmail.com>
2016-03-23 11:33:18 -0700

    Removed some old schema comments.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: posts
-#
-#  id         :integer         not null, primary key
-#  title      :string(255)
-#  contents   :text
-#  user_id    :integer
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
-#
-
 class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :contents, :title, :user_id, :img_url
