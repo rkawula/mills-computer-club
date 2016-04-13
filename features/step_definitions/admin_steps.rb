49bdffe2cd3346664a07d9546d60f49caeab233d
Rachel Kawula <rkawula@gmail.com>
2016-04-12 22:38:06 -0700

    Started getting cucumber & rspec ready for testing. Built a factory for users, started some features.

---
+++ features/step_definitions/admin_steps.rb
@@ -0,0 +1,19 @@
+Given(/^that I am logged in as administrator$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I visit the "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^there is an officer with name "([^"]*)", title "([^"]*)", and "([^"]*)" student$/) do |arg1, arg2, arg3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
