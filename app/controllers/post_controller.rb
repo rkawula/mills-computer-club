646bf888c22311edf0a92ad0f9a776307612552e
Rachel Kawula <rkawula@gmail.com>
2016-04-19 14:07:42 -0700

    Updated post views to display author, handle missing author, and display friendly created date.

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -10,5 +10,10 @@ class PostController < ApplicationController
 			flash[:warning] = "Sorry! That post is not yet published."
 			redirect_to post_index_path
 		end
+		begin
+			@author = User.find @post.user_id
+		rescue ActiveRecord::RecordNotFound
+			@author = nil
+		end
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image
   attr_accessible :contents, :title, :user_id, :img_url, :published
+
+  	def created_date_readable
+		created_at.to_formatted_s(:long_ordinal)
+	end
+	
 end
